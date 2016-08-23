5e1c07dc19c697ef5eb6fe234a2ebca03df81724
Kevin Jayamanne <Kevin Jayamanne>
2016-08-22 22:11:18 -0700

    Working on first turret impl

---
+++ Assets/scripts/TestTActionMachine.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestTActionMachine : MonoBehaviour, IntfTActionMachine {
+    public void findTarget()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void fireTurret()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void goActive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void goInactive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void rotateClockwise()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void rotateCounterClockwise()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/TestTStateMachine.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestTStateMachine : MonoBehaviour, IntfTStateMachine
+{
+    private double currAngle;
+    private MachineDefinitions.TState prevState;
+    private MachineDefinitions.TState state;
+    bool active;
+
+    public double getDirection()
+    {
+        return currAngle;
+    }
+
+    public MachineDefinitions.TState getPrevState()
+    {
+        return prevState;
+    }
+
+    public MachineDefinitions.TState getState()
+    {
+        return state;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        currAngle = 0;
+        prevState = MachineDefinitions.TState.Cooling;
+        state = MachineDefinitions.TState.Inactive;
+        active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        determineState();
+
+        print("State of turret is:" + state.ToString() + "\n");
+    }
+
+    private void determineState()
+    {
+        float h = Input.GetAxis("Horizontal");
+        if (Input.GetButton("Horizontal"))
+        {
+            if (h > 0)
+            {
+            }
+            // Holding Left
+            else if (h < 0)
+            {
+            }
+        }
+        if (Input.GetButton("Fire1"))
+        {
+            if( active == false )
+            {
+                active = true;
+            }
+        }
+    }
